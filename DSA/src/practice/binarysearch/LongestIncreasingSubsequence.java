f7fc8fda4975d2272befd904c8214c976fcd8dc5
ravi patel <aryanrdp@gmail.com>
2026-07-17 10:26:42 +0530

    DSA patterns + practice + interview ready

--- DSA/src/practice/binarysearch/LongestIncreasingSubsequence.java
+++ DSA/src/practice/binarysearch/LongestIncreasingSubsequence.java
@@ -48,4 +48,92 @@ public class LongestIncreasingSubsequence {
 
         return left;
     }
+
+        public int lengthOfLIS1(int[] nums) {
+            //        return lis(nums);
+//        return lisGenSeq(nums);
+            return lisGenSeqBs(nums);
+        }
+
+        private static int lis(int[]nums){
+
+            int[] dp = new int[nums.length];
+            Arrays.fill(dp, 1);
+
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+
+            int longest = 0;
+            for (int c: dp) {
+                longest = Math.max(longest, c);
+            }
+
+            return longest;
+        }
+
+        private static int lisGenSeq(int[]nums){
+            List<Integer> lst = new ArrayList<>();
+            lst.add(nums[0]);
+
+            for(int i=1;i<nums.length;i++){
+                int num = nums[i];
+                if(num > lst.get(lst.size()-1)){
+                    lst.add(num);
+                }else{
+                    int j=0;
+                    while(num > lst.get(j)){
+                        j+=1;
+                    }
+                    lst.set(j,num);
+                }
+            }
+
+            return lst.size();
+        }
+
+
+        private static int lisGenSeqBs(int[]nums){
+            List<Integer> lst = new ArrayList<>();
+            lst.add(nums[0]);
+
+            for(int i=1;i<nums.length;i++){
+                int num = nums[i];
+                if(num > lst.get(lst.size()-1)){
+                    lst.add(num);
+                }else{
+                    int j = binarySearch1(lst, num);
+                    lst.set(j,num);
+                }
+            }
+
+            return lst.size();
+        }
+
+        private static int binarySearch1(List<Integer> sub, int num) {
+            int left = 0;
+            int right = sub.size() - 1;
+            int mid = (left + right) / 2;
+
+            while (left < right) {
+                mid = (left + right) / 2;
+                if (sub.get(mid) == num) {
+                    return mid;
+                }
+
+                if (sub.get(mid) < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+
+
 }
